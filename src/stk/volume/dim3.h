7a61b8094f6ff7778cdfcf9a71b29c9a553aff89
Simon <simon.ekstrom@gmail.com>
2018-04-26 15:26:18 +0200

    dim3 type and tests

---
+++ src/stk/volume/dim3.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <stdint.h>
+#include <sstream>
+
+#include <stk/math/int3.h>
+
+#ifdef STK_USE_CUDA
+#include <vector_types.h>
+
+#else
+
+// Otherwise defined in vector_types.h for CUDA
+struct dim3
+{
+    uint32_t x;
+    uint32_t y;
+    uint32_t z;
+};
+
+#endif
+
+inline bool operator==(const dim3& l, const dim3& r)
+{
+    return (l.x == r.x && l.y == r.y && l.y == r.y && l.z == r.z);
+}
+inline bool operator!=(const dim3& l, const dim3& r)
+{
+    return !operator==(l, r);
+}
+
+inline std::ostream& operator<<(std::ostream& s, const dim3& v)
+{
+    s << '(' << v.x << ' ' << v.y << ' ' << v.z << ')';
+    return s;
+}
+
+namespace stk
+{
+    // Check whether the given point is inside the given range
+    inline bool is_inside(const dim3& dims, const int3& p)
+    {
+        return (p.x >= 0 && p.x < int(dims.x) && p.y >= 0 && p.y < int(dims.y) && p.z >= 0 && p.z < int(dims.z));
+    }
+}
+
---
+++ test/test_dim3.cpp
@@ -0,0 +1,35 @@
+#include "catch.hpp"
+
+#include <stk/volume/dim3.h>
+
+TEST_CASE("dim3 op", "[volume]")
+{
+    dim3 dims{10, 20, 30};
+
+    REQUIRE(dims == dim3{10, 20, 30});
+    REQUIRE(dims != dim3{99, 20, 30});
+    REQUIRE(dims != dim3{10, 99, 30});
+    REQUIRE(dims != dim3{10, 20, 99});
+}
+TEST_CASE("dim3_is_inside", "[volume]")
+{
+    dim3 dims{10, 10, 10};
+
+    REQUIRE(stk::is_inside(dims, int3{0,0,0}));
+    REQUIRE(stk::is_inside(dims, int3{5,5,5}));
+    
+    REQUIRE(!stk::is_inside(dims, int3{10,10,10}));
+    REQUIRE(!stk::is_inside(dims, int3{10,5,5}));
+    REQUIRE(!stk::is_inside(dims, int3{5,10,5}));
+    REQUIRE(!stk::is_inside(dims, int3{5,5,10}));
+
+    REQUIRE(!stk::is_inside(dims, int3{-5,5,5}));
+    REQUIRE(!stk::is_inside(dims, int3{5,-5,5}));
+    REQUIRE(!stk::is_inside(dims, int3{5,5,-5}));
+}
+TEST_CASE("dim3_to_string", "[volume]")
+{
+    std::ostringstream s;
+    s << dim3{10, 20, 30};
+    REQUIRE(s.str() == "(10 20 30)");
+}
