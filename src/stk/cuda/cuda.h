38892336f1b8bb70ea63e29f56d2a896322bb45d
Simon <simon.ekstrom@gmail.com>
2018-08-10 00:22:48 +0200

    CUDA specs for CUDA builds only

--- src/stk/cuda/cuda.h
+++ src/stk/cuda/cuda.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#ifdef __CUDACC__
+    #define CUDA_HOST_DEVICE __host__ __device__
+#else
+    #define CUDA_HOST_DEVICE
+#endif
+
 #ifdef STK_USE_CUDA
 
 #include <cuda_runtime.h>
--- src/stk/math/float4.h
+++ src/stk/math/float4.h
@@ -1,31 +1,32 @@
 #pragma once
 
+#include <stk/cuda/cuda.h>
 
-inline __host__ __device__ float4 operator+(const float4& l, const float4& r)
+inline CUDA_HOST_DEVICE float4 operator+(const float4& l, const float4& r)
 {
     return { l.x + r.x, l.y + r.y, l.z + r.z, l.w + r.w };
 }
-inline __host__ __device__ float4 operator-(const float4& l, const float4& r)
+inline CUDA_HOST_DEVICE float4 operator-(const float4& l, const float4& r)
 {
     return { l.x - r.x, l.y - r.y, l.z - r.z, l.w - r.w };
 }
 
 // element-wise multiplication
-inline __host__ __device__ float4 operator*(const float4& l, const float4& r)
+inline CUDA_HOST_DEVICE float4 operator*(const float4& l, const float4& r)
 {
     return { l.x * r.x, l.y * r.y, l.z * r.z, l.w * r.w };
 }
 // element-wise division
-inline __host__ __device__ float4 operator/(const float4& l, const float4& r)
+inline CUDA_HOST_DEVICE float4 operator/(const float4& l, const float4& r)
 {
     return { l.x / r.x, l.y / r.y, l.z / r.z, l.w / r.w };
 }
 
-inline __host__ __device__ float4 operator*(float l, const float4& r)
+inline CUDA_HOST_DEVICE float4 operator*(float l, const float4& r)
 {
     return { r.x * l, r.y * l, r.z * l, r.w * l };
 }
-inline __host__ __device__ float4 operator/(const float4& l, float r)
+inline CUDA_HOST_DEVICE float4 operator/(const float4& l, float r)
 {
     return { l.x / r, l.y / r, l.z / r, l.w / r };
 }
