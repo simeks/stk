be7010d0cebbde8ac55478866d4bda7f29643c65
Simon <simon.ekstrom@gmail.com>
2018-08-10 00:08:22 +0200

    CUDA friendly attributes for float4 operators

--- src/stk/math/float4.h
+++ src/stk/math/float4.h
@@ -1,31 +1,31 @@
 #pragma once
 
 
-inline float4 operator+(const float4& l, const float4& r)
+inline __host__ __device__ float4 operator+(const float4& l, const float4& r)
 {
     return { l.x + r.x, l.y + r.y, l.z + r.z, l.w + r.w };
 }
-inline float4 operator-(const float4& l, const float4& r)
+inline __host__ __device__ float4 operator-(const float4& l, const float4& r)
 {
     return { l.x - r.x, l.y - r.y, l.z - r.z, l.w - r.w };
 }
 
 // element-wise multiplication
-inline float4 operator*(const float4& l, const float4& r)
+inline __host__ __device__ float4 operator*(const float4& l, const float4& r)
 {
     return { l.x * r.x, l.y * r.y, l.z * r.z, l.w * r.w };
 }
 // element-wise division
-inline float4 operator/(const float4& l, const float4& r)
+inline __host__ __device__ float4 operator/(const float4& l, const float4& r)
 {
     return { l.x / r.x, l.y / r.y, l.z / r.z, l.w / r.w };
 }
 
-inline float4 operator*(float l, const float4& r)
+inline __host__ __device__ float4 operator*(float l, const float4& r)
 {
     return { r.x * l, r.y * l, r.z * l, r.w * l };
 }
-inline float4 operator/(const float4& l, float r)
+inline __host__ __device__ float4 operator/(const float4& l, float r)
 {
     return { l.x / r, l.y / r, l.z / r, l.w / r };
 }
