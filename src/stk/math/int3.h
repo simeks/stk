100f65078ebed89fafd7e4d049f92a69709beeea
Martino Pilia <martino.pilia@gmail.com>
2018-10-15 15:55:05 +0200

    convenience function

--- src/stk/math/int3.h
+++ src/stk/math/int3.h
@@ -31,3 +31,8 @@ inline int3 operator/(const int3& l, const int r)
 {
     return int3{l.x/r, l.y/r, l.z/r};
 }
+
+inline int3 make_int3(const float3& f)
+{
+    return int3{int(f.x), int(f.y), int(f.z)};
+}
