e7d3dfa2b482a7d7de773119e0a5abad9d4f0da1
Martino Pilia <martino.pilia@gmail.com>
2018-10-28 16:10:13 +0100

    Add optional out argument

--- src/stk/filters/map.h
+++ src/stk/filters/map.h
@@ -9,22 +9,28 @@ namespace stk {
 template<typename TVoxelInputType, typename TVoxelOutputType>
 VolumeHelper<TVoxelOutputType> map(
         const stk::VolumeHelper<TVoxelInputType>& image,
-        const std::function<TVoxelOutputType(TVoxelInputType)> op
+        const std::function<TVoxelOutputType(TVoxelInputType)> op,
+        VolumeHelper<TVoxelOutputType> *out = nullptr
         )
 {
-    VolumeHelper<TVoxelOutputType> out(image.size());
-    out.copy_meta_from(image);
+    VolumeHelper<TVoxelOutputType> dest;
+    if (!out) {
+        dest.allocate(image.size());
+        out = &dest;
+    }
+    ASSERT(out->size() == image.size());
+    out->copy_meta_from(image);
 
     #pragma omp parallel for
     for (int z = 0; z < (int) image.size().z; ++z) {
         for (int y = 0; y < (int) image.size().y; ++y) {
             for (int x = 0; x < (int) image.size().x; ++x) {
-                out(x, y, z) = op(image(x, y, z));
+                (*out)(x, y, z) = op(image(x, y, z));
             }
         }
     }
 
-    return out;
+    return *out;
 }
 
 } // namespace stk
