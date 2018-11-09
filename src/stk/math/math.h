1bb9893a7e505909bab09d3e848386cf5d90d22f
Martino Pilia <martino.pilia@gmail.com>
2018-11-09 14:43:04 +0100

    Prevent multiple definitions, track source files

--- src/stk/math/math.h
+++ src/stk/math/math.h
@@ -19,11 +19,7 @@ constexpr float eps()
 
 
 // Convert a Matrix3x3f to a row-major std::vector<float> representation
-std::vector<float> to_vector(const Matrix3x3f& o)
-{
-    const auto ptr = reinterpret_cast<const float*>(&o._rows);
-    return std::vector<float>(ptr, ptr + 9);
-}
+std::vector<float> to_vector(const Matrix3x3f& o);
 
 
 // Convert vector types to std::vector
