cfff61d5bf6fbc21b9681e5f274918980d23bdee
Simon <simon.ekstrom@gmail.com>
2019-10-29 12:56:35 +0100

    Iterator for dim3

--- src/stk/image/dim3.h
+++ src/stk/image/dim3.h
@@ -31,11 +31,65 @@ inline bool operator!=(const dim3& l, const dim3& r)
 {
     return !operator==(l, r);
 }
-
 inline std::ostream& operator<<(std::ostream& s, const dim3& v)
 {
     s << '(' << v.x << ' ' << v.y << ' ' << v.z << ')';
     return s;
+}
+
+/** Iterator to simplify iterating through a region.
+ * 
+ * Given a dim3, it holds a int3 pointing to the current position. Advancing
+ * the iterator pushes the pointer to the next position until reaching 
+ * {0, 0, dim3.z}, which is considered the end.
+ * 
+ * Example:
+ *  stk::Volume vol;
+ *  for (int3 p : vol.size()) {
+ *      vol(p) = 0:
+ *  }
+ * */
+struct Dim3Iterator
+{
+    dim3 _dim;
+    int3 _p;
+
+    Dim3Iterator(dim3 dim, int3 p) : _dim(dim), _p(p) {}
+
+    inline const int3& operator*()
+    {
+        return _p;
+    }
+    inline void operator++()
+    {
+        _p.x += 1;
+        if (_p.x == (int)_dim.x) {
+            _p.x = 0;
+            _p.y += 1;
+            if (_p.y == (int)_dim.y) {
+                _p.y = 0;
+                _p.z += 1;
+            }
+        }
+    }
+    inline bool operator!=(const Dim3Iterator& other)
+    {
+        return _dim != other._dim
+            || _p != other._p;
+    }
+};
+
+// Retrieves the begin iterator for Dim3Iterator.
+inline Dim3Iterator begin(const dim3& d)
+{
+    return Dim3Iterator(d, int3{0,0,0});
+}
+
+// Retrieves the end iterator for Dim3Iterator.
+//  p{0,0,d.z} is defined as the end.
+inline Dim3Iterator end(const dim3& d)
+{
+    return Dim3Iterator(d, int3{0,0, int(d.z)});
 }
 
 namespace stk
