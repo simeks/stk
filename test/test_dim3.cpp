13ff23031410c0ceb7a117ee7e4abe0ab33f09e0
Simon <simon.ekstrom@gmail.com>
2019-10-29 12:59:03 +0100

    Test for dim3 iterator

--- test/test_dim3.cpp
+++ test/test_dim3.cpp
@@ -33,3 +33,26 @@ TEST_CASE("dim3_to_string", "[volume]")
     s << dim3{10, 20, 30};
     REQUIRE(s.str() == "(10 20 30)");
 }
+TEST_CASE("dim3_iterator", "[volume]")
+{
+    dim3 d{3, 5, 7};
+    
+    int3 r_expected{0,0,0};
+    for (int z = 0; z < (int)d.z; ++z) {
+    for (int y = 0; y < (int)d.y; ++y) {
+    for (int x = 0; x < (int)d.x; ++x) {
+        r_expected.x += x;
+        r_expected.y += y;
+        r_expected.z += z;
+    }}}
+
+    int3 r{0,0,0};
+    for (int3 p : d) {
+        r.x += p.x;
+        r.y += p.y;
+        r.z += p.z;
+    }
+    REQUIRE(r.x == r_expected.x);
+    REQUIRE(r.y == r_expected.y);
+    REQUIRE(r.z == r_expected.z);
+}
