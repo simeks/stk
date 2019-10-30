0f39a957939f3b62b3ff33d8b166f7282eea7ab3
Simon <simon.ekstrom@gmail.com>
2019-10-30 16:45:50 +0100

    CUDA support for int3

--- src/stk/math/int3.h
+++ src/stk/math/int3.h
@@ -2,37 +2,40 @@
 
 #include "types.h"
 
+#include <stk/cuda/cuda.h>
+
 // Element-wise multiplication
-inline int3 operator*(const int3& l, const int3& r)
+inline CUDA_HOST_DEVICE int3 operator*(const int3& l, const int3& r)
 {
     return int3{l.x*r.x, l.y*r.y, l.z*r.z};
 }
-inline int3 operator/(const int3& l, const int3& r)
+inline CUDA_HOST_DEVICE int3 operator/(const int3& l, const int3& r)
 {
     return int3{l.x/r.x, l.y/r.y, l.z/r.z};
 }
-inline int3 operator+(const int3& l, const int3& r)
+inline CUDA_HOST_DEVICE int3 operator+(const int3& l, const int3& r)
 {
     return int3{l.x+r.x, l.y+r.y, l.z+r.z};
 }
-inline int3 operator-(const int3& l, const int3& r)
+inline CUDA_HOST_DEVICE int3 operator-(const int3& l, const int3& r)
 {
     return int3{l.x-r.x, l.y-r.y, l.z-r.z};
 }
-inline int3 operator*(const int l, const int3& r)
+inline CUDA_HOST_DEVICE int3 operator*(const int l, const int3& r)
 {
     return int3{l*r.x, l*r.y, l*r.z};
 }
-inline int3 operator*(const int3& l, const int r)
+inline CUDA_HOST_DEVICE int3 operator*(const int3& l, const int r)
 {
     return int3{r*l.x, r*l.y, r*l.z};
 }
-inline int3 operator/(const int3& l, const int r)
+inline CUDA_HOST_DEVICE int3 operator/(const int3& l, const int r)
 {
     return int3{l.x/r, l.y/r, l.z/r};
 }
 
-inline int3 make_int3(const float3& f)
+inline CUDA_HOST_DEVICE int3 make_int3(const float3& f)
 {
     return int3{int(f.x), int(f.y), int(f.z)};
 }
+
